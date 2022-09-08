e9eea57d5dacd45dffb73fb668a76dd53c6607f6
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-09-08 21:16:38 +0200

    minor optimization

--- bwt.py
+++ bwt.py
@@ -2,9 +2,11 @@ import copy
 import functools
 import random
 import string
+import time
 
 
 ALPHABET = ["$"] + list(string.ascii_letters) + [" "]
+ALPHABET_IDX = {c: i for i, c in enumerate(ALPHABET)}
 
 def transform(string_bytes: list[int], orderings=None, verbose=0, verbose_letters=None) -> list[int]:
     if (verbose >= 1):
@@ -15,11 +17,11 @@ def transform(string_bytes: list[int], orderings=None, verbose=0, verbose_letter
     for i in range(len(string_bytes)):
         bytes_matrix.append(string_bytes[i:len(string_bytes)] + string_bytes[:i])
     
-    if (verbose >= 1):
+    if (verbose >= 2):
         bytes_matrix_unsorted = copy.deepcopy(bytes_matrix)
 
-    bytes_matrix = sort_bytes_matrix(bytes_matrix, orderings=orderings)
-    if (verbose >= 1):
+    sort_bytes_matrix(bytes_matrix, orderings=orderings)
+    if (verbose >= 2):
         print("Unsorted                 Sorted")
         for bytes1, bytes2 in zip(bytes_matrix_unsorted, bytes_matrix):
             old_string = bytes_to_str(bytes1, verbose_letters, sep=' ')
@@ -43,7 +45,9 @@ def sort_bytes_matrix(bytes_matrix: list[list[int]], orderings=None) -> list[lis
     #print(orderings)
 
     # Sort according to 0 (epsilon="")
-    sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[""], start_idx=0)
+    ordering = orderings[""]
+    ordering_idxs = {i: k for i, k in enumerate(ordering)}
+    sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[""], ordering_idxs=ordering_idxs, start_idx=0)
     bytes_matrix.sort(key=functools.cmp_to_key(sop))
 
     # Sort according to 1, 2, ... n - 1
@@ -52,25 +56,28 @@ def sort_bytes_matrix(bytes_matrix: list[list[int]], orderings=None) -> list[lis
             orderings_keys = [key for key in orderings if key is not None and len(key) == i]
             
             for key in orderings_keys:
-                sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[key], start_idx=i)
+                ordering = orderings[key]
+                ordering_idxs = {i: k for i, k in enumerate(ordering)}
+                sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[key], ordering_idxs=ordering_idxs, start_idx=i)
                 bytes_matrix.sort(key=functools.cmp_to_key(sop))
 
     # Sort according to n (x=None)
     if (None in orderings):
-        sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[None], start_idx=i)
+        ordering = orderings[None]
+        ordering_idxs = {i: k for i, k in enumerate(ordering)}
+        sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[None], ordering_idxs=ordering_idxs, start_idx=i + 1) # i or i + 1
         bytes_matrix.sort(key=functools.cmp_to_key(sop))
 
-    return bytes_matrix
 
 
-def sort_ordering(bytes1, bytes2, ordering: list[int], start_idx=0) -> bool:
+def sort_ordering(bytes1, bytes2, ordering: list[int], ordering_idxs: dict[int, int], start_idx=0) -> bool:
     if (bytes1[:start_idx] != bytes2[:start_idx]):
         return 0
     
     for a, b in list(zip(bytes1, bytes2))[start_idx:]:
         if (a != b):
-            a_idx = ordering.index(a)
-            b_idx = ordering.index(b)
+            a_idx = ordering_idxs[a]
+            b_idx = ordering_idxs[b]
 
             return a_idx - b_idx
     return 0
@@ -100,26 +107,51 @@ if __name__ == "__main__":
 
 
     s = "mississippi$"
-    print(compute_runs_count(str_to_bytes(s)))
-    #s = "this string should be relatively easy to compress since there is probably a lot of repitions questionmark$"
-    #s = "this string is highly compressible highly compress string is high ly string which accepts the problem of compressibility"
-    #s = "aaababababaaabaaaabaaaabbbbaabaaaababbbbbc"
-    bytes = str_to_bytes(s, ALPHABET)
-
-    print("NORMAL BURROWS WHEELER")
+    bytes = str_to_bytes(s)
     bytes_t = transform(bytes, orderings=None, verbose=1)
-    print(bytes_t)
-    print(bytes_to_str(bytes_t))
-    print(f"Runs count: {compute_runs_count(bytes_t)}.")
+    #s = "this string should be relatively easy to compress since there is probably a lot of repitions questionmark$"
+    s = "this string is highly compressible highly compress string is high ly string which accepts the problem of compressibility$"
+    bytes = str_to_bytes(s)
+    bytes_t = transform(bytes, orderings=None, verbose=1)
+    #print(compute_runs_count(str_to_bytes(s)))
+    #s = "aaababababaaabaaaabaaaabbbbaabaaaababbbbbc"
 
+    time_start = time.time()
+
+    random.seed(2)
+
+    i = 0
     idx_list = list(range(len(ALPHABET)))
+    strs_possible = list(set(s))
     runs_count_min = 10e6
-    while True:
+    while i < 100:
         orderings = {"": idx_list}
-        for _ in range(20):
+        for _ in range(100):
             key = ""
-            while (random.choice([False, True])):
-                key += random.choice(list(set(s)))
+            while (random.choice([False, True, True])):
+                key += random.choice(strs_possible)
+            orderings[key] = random.sample(idx_list, len(idx_list))
+        orderings[None] = random.sample(idx_list, len(idx_list))
+
+        bytes_t = transform(bytes, orderings=orderings)
+        runs_count = compute_runs_count(bytes_t)
+        
+        runs_count_min = min(runs_count_min, runs_count)
+
+        i += 1
+
+    print(f"time diff: {time.time() - time_start}, run_min: {runs_count_min}")
+
+    """
+    idx_list = list(range(len(ALPHABET)))
+    strs_possible = list(set(s))
+    runs_count_min = 10e6
+    while random.choice([True for _ in range(100)] + [False]):
+        orderings = {"": idx_list}
+        for _ in range(50):
+            key = ""
+            while (random.choice([False, True, True])):
+                key += random.choice(strs_possible)
             orderings[key] = random.sample(idx_list, len(idx_list))
         orderings[None] = random.sample(idx_list, len(idx_list))
 
@@ -135,3 +167,4 @@ if __name__ == "__main__":
                 print("DONE")
 
     
+    """
