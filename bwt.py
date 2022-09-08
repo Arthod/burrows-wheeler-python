0a016a3a6810570554014067c243e23574b83831
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-09-08 17:56:06 +0200

    functioning? context-adaptive BWT

--- bwt.py
+++ bwt.py
@@ -33,37 +33,54 @@ def transform(string_bytes: list[int], orderings=None, verbose=0, verbose_letter
     
 def sort_bytes_matrix(bytes_matrix: list[list[int]], orderings=None) -> list[list[int]]:
     if (orderings is None):
-        # Ordering: epsilon = False, x = True, "aa" = "aa".
+        # Ordering: epsilon = "", x = None, "aa" = "aa".
         # "aa": [0, 1, 2, ...]
-        orderings = {True: [i for i in range(len(ALPHABET))]}
+        orderings = {"": [i for i in range(len(ALPHABET))]}
 
+    # Get orderings of each length, 1, 2, ... n - 1
     print(orderings)
-    matrix_subordering_idxs = []
 
-    # Sort according to 0 (epsilon=False)
-    sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, orderings[True])
+    # Sort according to 0 (epsilon="")
+    sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[""], start_idx=0)
     bytes_matrix.sort(key=functools.cmp_to_key(sop))
 
     # Sort according to 1, 2, ... n - 1
     for i in range(1, len(bytes_matrix[0])):
-        pass
+        orderings_keys = [key for key in orderings if key is not None and len(key) == i]
+        print(orderings_keys)
+        for key in orderings_keys:
+            sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[key], start_idx=i)
+            bytes_matrix.sort(key=functools.cmp_to_key(sop))
 
-    # Sort according to n (x=True)
+    # Sort according to n (x=None)
+    print(bytes_matrix[0])
+    print(orderings[None])
+    sop = lambda bytes1, bytes2: sort_ordering(bytes1, bytes2, ordering=orderings[None], start_idx=i)
+    bytes_matrix.sort(key=functools.cmp_to_key(sop))
 
     return bytes_matrix
 
 
-def sort_ordering(bytes1, bytes2, ordering: list[int]) -> bool:
-    for a, b in zip(bytes1, bytes2):
-        if (ordering.index(a) > ordering.index(b)):
-            return -1
-    return 1
+def sort_ordering(bytes1, bytes2, ordering: list[int], start_idx=0) -> bool:
+    if (bytes1[:start_idx] != bytes2[:start_idx]):
+        return 0
+    result = 1
+    for a, b in list(zip(bytes1, bytes2))[start_idx:]:
+        if (ordering.index(a) < ordering.index(b)): # TODO: Skal der være <=?
+            result = -1
+        else:
+            break
+    return result
 
 
-def str_to_bytes(s: str, letters) -> list[int]:
+def str_to_bytes(s: str, letters: list[str]=None) -> list[int]:
+    if (letters is None):
+        letters = ALPHABET
     return [letters.index(c) for c in s]
 
-def bytes_to_str(bytes: list[int], letters, sep="") -> str:
+def bytes_to_str(bytes: list[int], letters: list[str]=None, sep: str="") -> str:
+    if (letters is None):
+        letters = ALPHABET
     return sep.join(letters[b] for b in bytes)
 
 def reverse():
@@ -73,6 +90,6 @@ if __name__ == "__main__":
     s = "aabaaabac"
     bytes = str_to_bytes(s, ALPHABET)
 
-    orderings = {True: [1, 0, 2]}
+    orderings = {"": [1, 0, 2], "a": [2, 0, 1], "aa": [1, 0, 2], "aaba": [0, 2, 1], None: [0, 1, 2]}
     bytes_t = transform(bytes, orderings=orderings, verbose=1)
     print(bytes_t)
